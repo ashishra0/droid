908a6c6eafb9e251a4f24b7f1865fb9157979733
Ashish Rao <ashishrao2598@gmail.com>
2022-08-20 17:04:47 +0530

    integrate Notion

--- app/services/webhook_service.rb
+++ app/services/webhook_service.rb
@@ -1,20 +1,56 @@
 class WebhookService
+  include HTTParty
 
+  NOTION_URL = "https://api.notion.com/v1/pages"
+
+  attr_reader :webhook_params, :webhook_text
   def initialize(webhook_params)
     @webhook_params = webhook_params
+    @webhook_text = webhook_params["message"]["text"]
   end
 
   def process
-    if @webhook_params.nil?
-      {
-        status: :error,
-        message: "Webhook params are missing"
-      }
-    end
+    error_message if @webhook_params.blank?
 
+    notion_params = {
+      "parent": {
+        "database_id": ENV["NOTION_DATABASE_ID"],
+        "properties": {
+          "Name": {
+            "title": [
+              {
+                "text": {"content": webhook_text}
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    headers = {
+      "Authorization": "bearer #{ENV['NOTION_TOKEN']}",
+      "Content-Type": "application/json",
+      "Notion-Version": "2022-06-28"
+    }
+
+    HTTParty.post(NOTION_URL, body: notion_params.to_json, headers: headers)
+
+    success_message
+  end
+
+  private
+
+  def success_message
     {
       status: :success,
       message: "Webhook received"
+    }
+  end
+
+  def error_message
+    {
+      status: :error,
+      message: "Empty webhook params"
     }
   end
 end
